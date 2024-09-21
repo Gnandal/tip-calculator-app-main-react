e0da7fe164f026e22bb120032ff9b1d922ef8001
gnandal <egnandal500@yahoo.com>
2024-09-21 22:17:41 +0100

    Part 1

--- src/components/select-tip.tsx
+++ src/components/select-tip.tsx
@@ -7,16 +7,32 @@ export const SelectTip = () => {
     return <div class="grid grid-cols-3 grid-rows-2 w-full gap-6 text-center">{
         options.map((option, index) => {
             return (
-                <div className={`${option.checked?'bg-light-grayish-cyan':'bg-strong-cyan'} p-2.5 rounded-md cursor-pointer`}>
-                    <input
-                        type="radio"
-                        name="select"
-                        id={option.name}
-                        checked={option.checked}
-                        onChange={onChange.bind(undefined, index)} />
-                    <label className={option.checked ? 'text-strong-cyan' : 'text-white'} htmlFor={option.name}>{option.name}</label>
-                </div>
+                option.name === 'Custom' ?
+                    <div className={`${option.checked ? 'bg-white' : 'bg-light-grayish-cyan  p-2.5'} rounded-md cursor-pointer`}>
+                        <input
+                            type="radio"
+                            name="select-tip"
+                            id={option.name}
+                            checked={option.checked}
+                            onChange={onChange.bind(undefined, index)} />
+                        {
+                            option.checked ?
+                                <div className='w-full'>
+                                    <input className='w-full bg-very-light-grayish-cyan active:border-none active:outline-none p-2.5 text-end' type="number" id='bill' name='bill' defaultValue={'0'} onChange={onChange.bind(undefined, index)}/>
+                                </div> :
+                                <label htmlFor={option.name}>{option.name}</label>}
+                    </div> :
+
+                    <div className={`${option.checked ? 'bg-light-grayish-cyan' : 'bg-strong-cyan'} p-2.5 rounded-md cursor-pointer`}>
+                        <input
+                            type="radio"
+                            name="select-tip"
+                            id={option.name}
+                            checked={option.checked}
+                            onChange={onChange.bind(undefined, index)} />
+                        <label className={option.checked ? 'text-strong-cyan' : 'text-white'} htmlFor={option.name}>{option.name}</label>
+                    </div>
             )
-        })
-    }</div>;
+        })}
+    </div>;
 }
